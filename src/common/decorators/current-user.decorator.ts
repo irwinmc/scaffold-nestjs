fb5b47afdc8bd8219e7b9e4af0416434316cb421
Kelvin <kyia.x52@gmail.com>
2026-04-22 22:54:42 +0800

    fix(当前用户装饰器): 当用户未认证时抛出未授权异常
    添加对未认证用户的检查，当请求中没有用户信息时抛出 UnauthorizedException

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,4 +1,4 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 
 export interface RequestUser {
 	userId: string;
@@ -6,13 +6,13 @@ export interface RequestUser {
 	roles: string[];
 }
 
-export const CurrentUser = createParamDecorator((data: keyof RequestUser | undefined, ctx: ExecutionContext) => {
+export const CurrentUser = createParamDecorator<keyof RequestUser | undefined>((data, ctx: ExecutionContext) => {
 	const request = ctx.switchToHttp().getRequest();
 	const user = request.user as RequestUser | undefined;
 
-	if (data) {
-		return user?.[data];
+	if (!user) {
+		throw new UnauthorizedException('User not authenticated');
 	}
 
-	return user;
+	return data ? user[data] : user;
 });
